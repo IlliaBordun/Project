c4e32200b6a54a48bcf43776565620942966eaa7
illia <nudrob555@gmail.com>
2024-02-09 21:45:19 +0200

    some commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// some js
+// Находим кнопку по id
+const myButton = document.getElementById('my-button');
+
+// Добавляем обработчик события на клик
+myButton.addEventListener('click', () => {
+    // Выполняем запрос fetch при клике на кнопку
+    fetch('https://dream-singles.com/members/', {
+        mode: 'cors' // Требует соответствующей настройки на сервере
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Обрабатываем полученные данные
+        console.log(data);
+        // Добавьте свой код для обработки данных здесь
+    })
+    .catch(error => console.error('Ошибка:', error));
+});
